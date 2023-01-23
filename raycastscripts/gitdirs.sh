c68f62a63d6e41d6a2a219713c2157f5d4f7b797
Dean <gao.dean@hotmail.com>
2023-01-24 07:16:28 +1100

    update

--- raycastscripts/gitdirs.sh
+++ raycastscripts/gitdirs.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Update all gitdirs
+# @raycast.title Update All Gitdirs
 # @raycast.mode silent
 #
 # Optional parameters:
@@ -12,6 +12,7 @@ while read file
 do
 	dir=$(dirname file)
 	git -C "$dir" add -A
-	git -C "$dir" commit -m 'update' -q
+	git -C "$dir" commit -m 'update'
 	git -C "$dir" push -q
 done < ~/.config/shell/gitdirs
+echo "All gitdirs updated"
