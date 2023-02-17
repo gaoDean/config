3956b48454e7a579672f447f314654ea2f1c2180
Dean <gao.dean@hotmail.com>
2023-02-17 20:52:01 +1100

    update

--- raycastscripts/gitdirs.sh
+++ raycastscripts/gitdirs.sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Update All Gitdirs
-# @raycast.mode fullOutput
+# @raycast.mode silent
 #
 # Optional parameters:
 # @raycast.icon 📄
