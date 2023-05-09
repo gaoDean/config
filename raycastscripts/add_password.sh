d2e4767c9067a63f35d994649ff87a07692c35b0
Dean <gao.dean@hotmail.com>
2023-05-09 20:51:08 +1000

    update: Tue May  9 20:51:07 AEST 2023

--- raycastscripts/add_password.sh
+++ raycastscripts/add_password.sh
@@ -8,10 +8,12 @@
 #
 # Optional parameters:
 # @raycast.icon 🗝️
-# @raycast.argument1 { "type": "text", "placeholder": "Account", "optional": false }
+# @raycast.argument1 { "type": "text", "placeholder": "Site", "optional": false }
+# @raycast.argument2 { "type": "text", "placeholder": "Account", "optional": true }
+# @raycast.argument2 { "type": "text", "placeholder": "Password", "optional": false }
 
 if [ $# -eq "0" ]; then
 	echo "No arguments"
 	exit 0
 fi
-~/repos/bin/pa/pa add "$1"
+emacs --batch --eval "(pa--add $1 $2 $3)"
--- raycastscripts/showpassword.sh
+++ raycastscripts/showpassword.sh
@@ -9,7 +9,23 @@
 # @raycast.icon 🔑
 
 killall -q choose
-password=$(~/repos/bin/pa/pa list | choose)
+password=$(~/repos/bin/pa/pa list |
+               awk -F':' '
+BEGIN { max_len = 0 }
+{
+  left=$1; right=$2;
+  if (length(left) > max_len) {
+    max_len = length(left);
+  }
+  lines[NR] = left ":" right;
+}
+END {
+  for (i = 1; i <= NR; i++) {
+    split(lines[i], parts, ":");
+    left = parts[1]; right = parts[2];
+    printf "%-" max_len+1 "s%s\n", left, right;
+  }
+}' | choose | gsed -E 's/^(\S+)\s+(\S+)$/\1:\2/')
 if [ -n "$password" ]; then
 	username=$(echo "$password" | cut -d: -f 2)
 	if [ -n "$username" ]; then
